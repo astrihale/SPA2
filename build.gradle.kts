9e4c1d818e79034c90562dbc3ba2f9573cf67c54
Astrihale <nenadvvvvvvv@gmail.com>
2020-10-21 22:50:06 +0200

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "astrihale"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
---
+++ src/main/java/astrihale/SPA2/Sortiranje.java
@@ -0,0 +1,8 @@
+package astrihale.SPA2;
+
+class Sortiranje {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
