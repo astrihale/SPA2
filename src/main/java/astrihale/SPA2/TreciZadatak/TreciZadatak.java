65c4665d406bd7be70ff4fd07be66a928e7b0648
Astrihale <nenadvvvvvvv@gmail.com>
2020-11-18 13:45:19 +0100

    StatSet for first class ever

--- src/main/java/astrihale/SPA2/TreciZadatak/TreciZadatak.java
+++ src/main/java/astrihale/SPA2/TreciZadatak/TreciZadatak.java
@@ -1,11 +1,35 @@
 package astrihale.SPA2.TreciZadatak;
 
-import org.svetovid.Svetovid;
-import org.svetovid.io.SvetovidReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class TreciZadatak {
 
     public static void main(String[] args) {
-        SvetovidReader reader;
+        // Proveriti odmah da su svi argumenti prosledjeni
+        if (args.length < 2) {
+            throw new IllegalArgumentException(
+                    "Niste prosledili argumente za putanje do oba fajla.");
+        }
+
+        // Procesirati sve odmah
+        String putanjaZa2019 = args[0];
+        String putanjaZa2020 = args[1];
+
+
+        // Ucitaj fudbalere
+        Fudbaler[] fudbaleri2019;
+        Fudbaler[] fudbaleri2020;
+        try {
+            fudbaleri2019 = Fudbaler.ucitajSveIzFajla(putanjaZa2019);
+            fudbaleri2020 = Fudbaler.ucitajSveIzFajla(putanjaZa2020);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Fajl koji je prosledjen za citanje nije dostupan!");
+        }
+
+        StatSet<Fudbaler> setFudbalera = new StatSet<>();
+        setFudbalera.addAll(Arrays.asList(fudbaleri2019));
+        setFudbalera.printStats();
     }
 }
